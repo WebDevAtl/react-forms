7d822c0b79ad34f62d9c63e737e0559b252f235a
Noah Rorabaugh <noahrorabaugh@50-201-202-146-static.hfc.comcastbusiness.net>
2020-01-08 20:34:30 -0500

    Dropdown component

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -7,7 +7,17 @@ export default class Dropdown extends Component {
     render() {
         return (
             <div>
-                
+                <form>
+                    <select name='fruits' multiple>
+                        <option>Apple</option>
+                        <option>Bananas</option>
+                        <option>Cantaloupe</option>
+                        <option>Grapes</option>
+                        <option>Lemons</option>
+                        <option>Muscadines</option>
+                        <option>Pears</option>
+                    </select>
+                </form>
             </div>
         )
     }
