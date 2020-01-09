e845556d8cbe762edeff7e32afe1f34e5e4aeb9e
Noah Rorabaugh <noahrorabaugh@50-201-202-146-static.hfc.comcastbusiness.net>
2020-01-08 20:51:56 -0500

    Dropdown attempt

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,20 +1,32 @@
 import React, { Component } from 'react'
 
 export default class Dropdown extends Component {
+    state = {
+        open: false
+    }
+
+    drop = () => {
+        let previousState = {...this.state}
+        previousState.open=!this.state.open
+        this.setState({previousState})
+    }
+
     render() {
         return (
             <div>
+                {this.state.open? <div style='{margin: 0 auto; border: 1px solid black; border-radius: 5px}' onClick={this.drop}>Fruits</div>:
                 <form>
-                    <select name='fruits' multiple>
-                        <option>Apple</option>
-                        <option>Bananas</option>
-                        <option>Cantaloupe</option>
-                        <option>Grapes</option>
-                        <option>Lemons</option>
-                        <option>Muscadines</option>
-                        <option>Pears</option>
-                    </select>
+                <select name='fruits' multiple>
+                    <option>Apple</option>
+                    <option>Bananas</option>
+                    <option>Cantaloupe</option>
+                    <option>Grapes</option>
+                    <option>Lemons</option>
+                    <option>Muscadines</option>
+                    <option>Pears</option>
+                </select>
                 </form>
+                }
             </div>
         )
     }
